8594c4e2da0509763cd41f5304968bf0e5d43a4e
PanickingLynx <joshuaweber2001@gmail.com>
2020-01-09 11:46:30 +0100

    Added hotbar credits and finalized automatic name variation

--- akuma.py
+++ akuma.py
@@ -6,8 +6,9 @@ import sys
 import requests
 import pymongo
 from bs4 import BeautifulSoup
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap
 import qdarkstyle
 from ui import Ui_QMainWindow
 
@@ -30,21 +31,21 @@ MyDB = MyClient["AkumaPy"]
 mycol = MyDB["links"]
 
 def trigger():
+    w.ui.creditsTrigger.triggered.connect(lambda: showCredits())
     w.ui.go.clicked.connect(lambda: hunt())
 
 #Sample DB Search Criteria
 def namevariation(name, field):
-    y = 0
-    newname = [""]
-    respaced = [""]
+    respaced = []
+    newname = []
     if w.ui.namepath.text() == "":
         w.ui.textEdit.clear()
-        w.ui.textEdit.setText("ERROR. Please give a path to a .txt File for\n automatic name variation.\n This can also be empty.\n")
+        w.ui.textEdit.setText("ERROR. Please give a path to a .txt File for\n automatic name variation.\n")
         trigger()
     path = open(w.ui.namepath.text(), "r")
     lines = path.readlines()
     x = 0
-    y = 1
+    y = 0
     prefix = ""
     suffix = ""
     for i in lines:
@@ -52,14 +53,15 @@ def namevariation(name, field):
             suffix = lines[x]
         elif "*" in lines[x]:
             prefix = lines[x]
-        newname.append(prefix + name + suffix)
+        newname.insert(x, prefix + name + suffix)
         carded = re.sub("WILDCARD", newname[y], field["link"])
         modified = re.sub("\s", "_", carded)
         modified = re.sub(re.escape("*"), "", modified)
         modified = re.sub(re.escape(";"), "", modified)
-        respaced.append(re.sub("\n", "", modified))
+        respaced.insert(x, re.sub("\n", "", modified))
         x = x + 1
         y = y + 1
+        print(respaced)
     
     return respaced
 
@@ -67,17 +69,28 @@ def namevariation(name, field):
 
 
 def hunt():
+    mainname = [""]
+    mainlink = [""]
     name = w.ui.usernameIn.text()
     output = ""
     pathToLog = w.ui.filePath.text()
     
     for field in mycol.find({}, {'_id': 0, 'name': 1, 'link': 1, 'type': 1}):
-        z = 1
-        for i in namevariation(name, field):
-            if w.ui.useNameVar.isChecked:
-                mainlink = namevariation(name, field)[z]
+        z = 0
+        print(field["link"])
+        if w.ui.useNameVar.isChecked():
+            mainname.clear()
+            mainname = namevariation(name, field)
+        else:
+            mainname.clear()
+            mainname.insert(0, name)
+            print(mainname)
+        for i in mainname:
+            if w.ui.useNameVar.isChecked():
+                mainlink = namevariation(name, field)
             else:
-                mainlink = field["link"] + name
+                mainlink.clear()
+                mainlink.insert(0, re.sub("WILDCARD", mainname[z], field["link"]))
             json.dumps(field)
             wname = field["name"]
             wtype = field["type"]
@@ -86,67 +99,85 @@ def hunt():
                 output = output + "\nCENSORED\n"
                 z = z + 1
             else:
-                req = requests.get(mainlink).status_code
+                print(mainlink)
+                req = requests.get(mainlink[z]).status_code
                 output = output + "--------------------\n"
                 output = output + "\n" + wname + "\n"
-                output = output + mainlink + "\n"
+                output = output + mainlink[z] + "\n"
                 output = statuscheck(req, output, mainlink, pathToLog)
                 z = z + 1
 
 
 
-def statuscheck(req, output, respaced, pathToLog):
+def statuscheck(req, output, mainlink, pathToLog):
     y = 0
-    if req == 200:
-        output = output + "200 OK!\n"
-    elif req == 503:
-        output = output + "503 ERROR!\n"
-    elif req == 403:
-        output = output + "403 DENIED!\n"
-    elif req == 404:
-        output = output + "404 MISSING!\n"
-    elif req == 301:
-        output = output + "301 MOVED!\n"
-    else:
-        output = output + "TIMEOUT OR DOWN!\n"
+    for i in mainlink:
+        if req == 200:
+            output = output + "200 OK!\n"
+        elif req == 503:
+            output = output + "503 ERROR!\n"
+        elif req == 403:
+            output = output + "403 DENIED!\n"
+        elif req == 404:
+            output = output + "404 MISSING!\n"
+        elif req == 301:
+            output = output + "301 MOVED!\n"
+        else:
+
+            output = output + "TIMEOUT OR DOWN!\n"
             
 
-    #Look for Data suspicious of a missing profile in the html document
-    page = requests.get(respaced)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    status = soup.find('html').extract()
-    status = status.text.lower()
-    #Add entry
+        #Look for Data suspicious of a missing profile in the html document
+        print(mainlink)
+        page = requests.get(mainlink[y])
+        soup = BeautifulSoup(page.text, 'html.parser')
+        status = soup.find('html').extract()
+        status = status.text.lower()
+        #Add entry
 
-    if w.ui.saveHTML.isChecked():
-        #Save data from custom html tag given from UI if the option is checked
-        soup2 = BeautifulSoup(page.text, 'html.parser')
-        deeptext = w.ui.htmlTags.text()
-        htmltext = soup2.find(deeptext).extract()
-        htmlFile = open(w.ui.pathToHTML.text(), "w")
-        htmlFile.write(str(htmltext))
-        htmlFile.close()
+        if w.ui.saveHTML.isChecked():
+            #Save data from custom html tag given from UI if the option is checked
+            soup2 = BeautifulSoup(page.text, 'html.parser')
+            deeptext = w.ui.htmlTags.text()
+            htmltext = soup2.find(deeptext).extract()
+            htmlFile = open(w.ui.pathToHTML.text(), "w")
+            htmlFile.write(str(htmltext))
+            htmlFile.close()
 
-    if status is not None: 
-        #If the website gave back a HTTP Status code, check for words suspicious of a missing page
-        if "not found" in status or "missing" in status or "oops" in status or "removed" in status or "nicht gefunden" in status or "fehlt" in status or "ups" in status or "entfernt" in status:
-            output = output + "FAILED TO FIND!\n"
+        if status is not None: 
+            #If the website gave back a HTTP Status code, check for words suspicious of a missing page
+            if "not found" in status or "missing" in status or "oops" in status or "removed" in status or "nicht gefunden" in status or "fehlt" in status or "ups" in status or "entfernt" in status:
+                output = output + "FAILED TO FIND!\n"
+            else:
+                output = output + "PROBABLY EXISTS!\n"
         else:
-            output = output + "PROBABLY EXISTS!\n"
-    else:
-        output = output + "EMPTY TITLE CODE MAYBE DOWN OR BAD HTML?\n"
-    y = y + 1
-    #Give output to the main Log field
-    w.ui.textEdit.setText(output) 
-    #Write to a textfile if wanted
-    if w.ui.textFileRadio.isChecked():
-        outFileText = open(pathToLog, "w")
-        outFileText.write(output)
-        outFileText.close()
-    return output
+            output = output + "EMPTY TITLE CODE MAYBE DOWN OR BAD HTML?\n"
+        y = y + 1
+        #Give output to the main Log field
+        w.ui.textEdit.setText(output) 
+        #Write to a textfile if wanted
+        if w.ui.textFileRadio.isChecked():
+            outFileText = open(pathToLog, "w")
+            outFileText.write(output)
+            outFileText.close()
+        return output
     #JSON Output will go here
 
+def showCredits():
+    msg = QMessageBox()
+    msg.setIconPixmap(QPixmap("./mainicon.png"))
+    msg.setText("People who showed me their Support:")
+    msg.setWindowTitle("Credits")
+    msg.setInformativeText("- Doelicious (Testing) \n- Maze aka. Black_eks (Script Icon artwork)\n")
+    msg.setEscapeButton(msg.Ok)
+    retval = msg.exec()
+    
+    if retval == msg.Ok:
+        trigger()
+    
+
 #Wait for start trigger
+w.ui.creditsTrigger.triggered.connect(lambda: showCredits())
 w.ui.go.clicked.connect(lambda: hunt())
 w.show()
 sys.exit(app.exec_())

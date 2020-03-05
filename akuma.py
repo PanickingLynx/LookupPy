0f6f647e4846a0301950fe14a2175c51d0056ea8
PanickingLynx <joshuaweber2001@gmail.com>
2020-03-05 09:40:44 +0100

    Added colored terminal prompts and os detection

--- akuma.py
+++ akuma.py
@@ -3,6 +3,7 @@
 import json
 import re
 import sys
+import platform
 import requests
 import pymongo
 from bs4 import BeautifulSoup
@@ -12,6 +13,20 @@ from PyQt5.QtGui import QPixmap
 import qdarkstyle
 from ui import Ui_QMainWindow
 from databaseInsertion import Ui_databaseInsertion
+from termcolor import colored
+
+def osDetection():
+    currentSystem = platform.system()
+    if currentSystem == "Windows":
+        print(colored("WRONG OPERATING SYSTEM! PLEASE USE LINUX!", "red"))
+        input("Press RETURN to exit")
+        exit()
+    if currentSystem == "Darwin":
+        print(colored("WRONG OPERATING SYSTEM! PLEASE USE LINUX!", "red"))
+        input("Press RETURN to exit")
+        exit()
+
+osDetection()
 
 class AppWindow(QMainWindow):
     def __init__(self):
@@ -31,7 +46,7 @@ def newTorSession():
     newProxy = requests.session()
     newProxy.proxies = { 'http': 'socks5h://localhost:9050',
                         'https': 'socks5h://localhost:9050'}
-    print("Starting a new Tor proxy session....")
+    print(colored("Starting a new Tor proxy session....", "magenta"))
     return newProxy
 
 session = newTorSession()
@@ -52,19 +67,19 @@ w = AppWindow()
 MyClient = pymongo.MongoClient("mongodb://localhost:27017/")
 MyDB = MyClient["AkumaPy"]
 mycol = MyDB["links"]
-print("Getting current MongoDB state...")
+print(colored("Getting current MongoDB state...", "yellow"))
 
 def trigger():
     w.ui.creditsTrigger.triggered.connect(lambda: showCredits())
     w.ui.go.clicked.connect(lambda: hunt())
     w.ui.insertionTrigger.triggered.connect(lambda: insertToDatabase())
-    print("Waiting for another run...")
+    print(colored("Waiting for another run...", "yellow"))
 
 def insertToDatabase():
     d.show()
     d.accepted.connect(lambda: push())
     d.rejected.connect(lambda: trigger())
-    print("Showing database insertion...")
+    print(colored("Showing database insertion...", "green"))
 
 def push():
     siteName = d.databaseInsertion.nameOfSite.text()
@@ -80,9 +95,9 @@ def push():
         "link": siteLink,
         "type": siteIsNSFW
         }
-    print("Inserting new JSON to database...")
+    print(colored("Inserting new JSON to database...", "yellow"))
     mycol.insert_one(newsite)
-    print("Inserted!")
+    colored("Inserted!", "green")
     trigger()
 
 
@@ -93,7 +108,7 @@ def namevariation(name, field):
     if w.ui.namepath.text() == "":
         w.ui.textEdit.clear()
         w.ui.textEdit.setText("ERROR. Please give a path to a .txt File for\n automatic name variation.\n")
-        print("ERROR! Exiting namevariation check....")
+        print(colored("ERROR! Exiting namevariation check....", "red"))
         trigger()
     path = open(w.ui.namepath.text(), "r")
     lines = path.readlines()
@@ -104,17 +119,17 @@ def namevariation(name, field):
     for i in lines:
         if ";" in lines[x]:
             suffix = lines[x]
-            print("Found a suffix...")
+            print(colored("Found a suffix...", "yellow"))
         elif "*" in lines[x]:
             prefix = lines[x]
-            print("Found a prefix...")
+            print(colored("Found a prefix...", "yellow"))
         newname.insert(x, prefix + name + suffix)
         carded = re.sub("WILDCARD", newname[y], field["link"])
         modified = re.sub("\s", "", carded)
         modified = re.sub(re.escape("*"), "", modified)
         modified = re.sub(re.escape(";"), "", modified)
         respaced.insert(x, re.sub("\n", "", modified))
-        print("Making one name...")
+        print(colored("Making one name...", "yellow"))
         x = x + 1
         y = y + 1
 
@@ -122,14 +137,14 @@ def namevariation(name, field):
     y = y - 1
     if ";" in lines[x]:
         suffix = lines[x]
-    print("Mutating the name...")
+    print(colored("Mutating the name...", "yellow"))
     newname.insert(x, name + suffix)
     carded = re.sub("WILDCARD", newname[y], field["link"])
     modified = re.sub("\s", "", carded)
     modified = re.sub(re.escape("*"), "", modified)
     modified = re.sub(re.escape(";"), "", modified)
     respaced.insert(x, re.sub("\n", "", modified))
-    print("DONE!")
+    print(colored("DONE!", "green"))
     return respaced
 
 
@@ -140,21 +155,21 @@ def hunt():
     name = w.ui.usernameIn.text()
     output = ""
     pathToLog = "./{}.txt".format(w.ui.usernameIn.text())
-    print("Starting the hunt...")
+    print(colored("Starting the hunt...", "green"))
     for field in mycol.find({}, {'_id': 0, 'name': 1, 'link': 1, 'type': 1}):
-        print("Grabbing one dataset from the database....")
+        print(colored("Grabbing one dataset from the database....", "yellow"))
         z = 0
         if w.ui.useNameVar.isChecked():
-            print("Adding namevariation parameter... LETS MUTATE!")
+            print(colored("Adding namevariation parameter... LETS MUTATE!", "green"))
             mainname.clear()
             mainname = namevariation(name, field)
         else:
-            print("No namevariation selected... SKIPPING!")
+            print(colored("No namevariation selected... SKIPPING!", "cyan"))
             mainname.clear()
             mainname.insert(0, name)
         for i in mainname:
             if w.ui.useNameVar.isChecked():
-                print("Starting namevariation...")
+                print(colored("Starting namevariation...", "yellow"))
                 mainlink = namevariation(name, field)
             else:
                 mainlink.clear()
@@ -163,26 +178,26 @@ def hunt():
             wname = field["name"]
             wtype = field["type"]
             if w.ui.checkNSFWService.isChecked() != True and wtype == 1:
-                print("Censoring one result... Guess you're at work... Or in China...")
+                print(colored("Censoring one result... Guess you're at work... Or in China...", "red"))
                 output = output + "--------------------\n"
                 output = output + "\nCENSORED\n"
                 z = z + 1
             else:
                 if w.ui.onTor.isChecked():
-                    print("Getting new Tor session... Let's go dark...")
+                    print(colored("Getting new Tor session... Let's go dark...", "magenta"))
                     session = newTorSession()
-                    print("Getting anonymous request.... Getting statuscode")
+                    print(colored("Getting anonymous request.... Getting statuscode", "yellow"))
                     req = session.get(mainlink[z]).status_code
-                    print("DONE!")
+                    print(colored("DONE!", "green"))
                 else:
-                    print("Grabbing new statuscode")
+                    print(colored("Grabbing new statuscode...", "yellow"))
                     req = requests.get(mainlink[z]).status_code
-                    print("DONE!")
-                print("Collecting new sampled output data...")
+                    print(colored("DONE!", "green"))
+                print(colored("Collecting new sampled output data...", "yellow"))
                 output = output + "--------------------\n"
                 output = output + "\n" + wname + "\n"
                 output = output + mainlink[z] + "\n"
-                print("Translating statuscode....")
+                print(colored("Translating statuscode....", "yellow"))
                 output = statuscheck(req, output, mainlink, pathToLog, mainname)
                 z = z + 1
 
@@ -191,7 +206,7 @@ def hunt():
 def statuscheck(req, output, mainlink, pathToLog, mainname):
     y = 0
     for i in mainlink:
-        print("Testing....")
+        print(colored("Testing....", "yellow"))
         if req == 200:
             output = output + "200 OK!\n"
         elif req == 503:
@@ -204,65 +219,65 @@ def statuscheck(req, output, mainlink, pathToLog, mainname):
             output = output + "301 MOVED!\n"
         else:
             output = output + "TIMEOUT OR DOWN!\n"
-        print("DONE!")
+        print(colored("DONE!", "green"))
         #Look for Data suspicious of a missing profile in the html document
         if w.ui.onTor.isChecked():
-            print("Getting new Tor session... Let's go dark... (again)")
+            print(colored("Getting new Tor session... Let's go dark... (again)", "magenta"))
             session = newTorSession()
-            print("Getting HTML document anonymously....")
+            print(colored("Getting HTML document anonymously....", "yellow"))
             page = session.get(mainlink[y])
-            print("DONE!")
+            print(colored("DONE!", "green"))
         else:
-            print("Getting HTML document...")
+            print(colored("Getting HTML document...", "yellow"))
             page = requests.get(mainlink[y])
-            print("DONE!")
-        print("Parsing HTML...")
+            print(colored("DONE!", "green"))
+        print(colored("Parsing HTML...", "yellow"))
         soup = BeautifulSoup(page.text, 'html.parser')
-        print("Looking for title...")
+        print(colored("Looking for title...", "yellow"))
         status = soup.find('title').extract()
-        print("Lowering title...")
+        print(colored("Lowering title...", "yellow"))
         status = status.text.lower()
         #Add entry
 
         if w.ui.saveHTML.isChecked():
-            print("Saving to HTML document...")
+            print(colored("Saving to HTML document...", "yellow"))
             #Save data from custom html tag given from UI if the option is checked
             htmlFile = open("./{}.html".format(w.ui.usernameIn.text()), "a")
             soup2 = BeautifulSoup(page.text, 'html.parser')
             deeptext = w.ui.htmlTags.text()
             htmltext = soup2.find(deeptext).extract()
             htmlFile.write(str(htmltext))
-            print("DONE!")
+            print(colored("DONE!", "green"))
             htmlFile.close()
 
         if status is not None:
-            print("Found active HTTP Status code...")
+            print(colored("Found active HTTP Status code...", "yellow"))
             #If the website gave back a HTTP Status code, check for words suspicious of a missing page
             if "not found" in status or "missing" in status or "oops" in status or "removed" in status or "nicht gefunden" in status or "fehlt" in status or "ups" in status or "entfernt" in status or "existiert nicht" in status or "doesn't exist" in status:
-                print("Guessing that page may not exist...")
+                print(colored("Guessing that page may not exist...", "yellow"))
                 output = output + "FAILED TO FIND!\n"
                 hit = "bad"
             else:
-                print("Guessing that page probably exists...")
+                print(colored("Guessing that page probably exists...", "yellow"))
                 output = output + "PROBABLY EXISTS!\n"
                 hit = "good"
         else:
-            print("FATAL ERROR! Server might be down...")
+            print(colored("FATAL ERROR! Server might be down...", "red"))
             output = output + "EMPTY TITLE CODE MAYBE DOWN OR BAD HTML?\n"
             hit = "error"
         y = y + 1
         #Give output to the main Log field
-        print("Outputting data....")
+        print(colored("Outputting data....", "yellow"))
         w.ui.textEdit.setText(output)
         #Write to a textfile if wanted
         if w.ui.textFileRadio.isChecked():
-            print("Writing to textfile in root directory...")
+            print(colored("Writing to textfile in root directory...", "yellow"))
             outFileText = open(pathToLog, "w")
             outFileText.write(output)
             outFileText.close()
-            print("DONE!")
+            print(colored("DONE!", "green"))
         if w.ui.jsonFileRadio.isChecked():
-            print("Writing to JSON file in root directory...")
+            print(colored("Writing to JSON file in root directory...", "yellow"))
             toJSON = {
                 "username": mainname,
                 "link": mainlink,
@@ -277,12 +292,12 @@ def statuscheck(req, output, mainlink, pathToLog, mainname):
             jfile = open(pathToLog, "a")
             jfile.write(textJSON)
             jfile.close()
-            print("DONE!")
+            print(colored("DONE!", "green"))
         return output
 
 
 def showCredits():
-    print("Showing credits... THANKS FOR TAKING A LOOK <3")
+    print(colored("Showing credits... THANKS FOR TAKING A LOOK <3", "green"))
     msg = QMessageBox()
     msg.setIconPixmap(QPixmap("./mainicon.png"))
     msg.setText("People who showed me their Support:")
@@ -292,7 +307,7 @@ def showCredits():
     retval = msg.exec()
     if retval == msg.Ok:
         trigger()
-        print("Have fun using!")
+        print(colored("Have fun using!", "green"))
 
 #Wait for start trigger
 w.ui.creditsTrigger.triggered.connect(lambda: showCredits())
@@ -300,6 +315,6 @@ w.ui.insertionTrigger.triggered.connect(lambda: insertToDatabase())
 w.ui.go.clicked.connect(lambda: hunt())
 w.show()
 d.hide()
-print("READY!")
+print(colored("READY!", "green"))
 sys.exit(app.exec_())
 sys.exit(DBInsertion.exec_())
